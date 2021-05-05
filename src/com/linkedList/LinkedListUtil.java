b2aba168054ee2b45304ba7505857b040eace833
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-05 18:59:08 +0530

    Added:To create simple Linked List using Append method

--- src/com/linkedList/LinkedListUtil.java
+++ src/com/linkedList/LinkedListUtil.java
@@ -7,10 +7,18 @@ public class LinkedListUtil {
 	public static  void main(String[] args) {
 		LinkedList linkedlist = new LinkedList();
 		linkedlist.push(70);
-	    linkedlist.push(30);
+	        linkedlist.push(30);
 		linkedlist.push(56);
 		
 		linkedlist.printList();
+		System.out.println("\n");
+		
+		LinkedList linkedlist1 = new LinkedList();
+		linkedlist1.append(70);
+	        linkedlist1.append(30);
+		linkedlist1.append(56);
+		
+		linkedlist1.printList();
 		
 	}
 }
@@ -18,17 +26,16 @@ public class LinkedListUtil {
 class LinkedList {
      Node head;
 	
-	 class Node {
+     class Node {
 		
-		 int data;
-		 Node next;
+	     int data;
+	     Node next;
 	
-		 Node(int data){
-		   this.data = data;
-		   next =  null;
-		 }
-		
-	 }
+	     Node(int data){
+	       this.data = data;
+	       next =  null;
+	     }
+     }
 
      public void push (int data) {
  	     Node newNode=new Node(data);
@@ -47,4 +54,19 @@ class LinkedList {
  		  temp = temp.next;
  	     }
      }
+     
+     public void append(int data) {
+    	     Node newNode = new Node(data);
+    	     if(head == null) {
+    		 head= newNode;
+    		 return;
+    	     }
+    	 
+    	     Node last= head;
+    	     while(last.next != null) {
+    		 last = last.next;
+    	     }
+    	     last.next=newNode;
+     }
+     
 }
