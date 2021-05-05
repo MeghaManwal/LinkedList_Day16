039709cb858c67c815858ba322662e2b1c323e1e
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-05 18:49:44 +0530

    Added:To create simple Linked List using Push method

--- src/com/linkedList/LinkedListUtil.java
+++ src/com/linkedList/LinkedListUtil.java
@@ -1,17 +1,50 @@
 package com.linkedList;
 
-
-import java.util.LinkedList;
+import java.util.List;
 
 public class LinkedListUtil {
 	
 	public static  void main(String[] args) {
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(30);
-	        list.add(56);
-		list.add(70);
+		LinkedList linkedlist = new LinkedList();
+		linkedlist.push(70);
+	    linkedlist.push(30);
+		linkedlist.push(56);
 		
-		list.forEach(System.out::println);
+		linkedlist.printList();
 		
 	}
+}
+
+class LinkedList {
+     Node head;
+	
+	 class Node {
+		
+		 int data;
+		 Node next;
+	
+		 Node(int data){
+		   this.data = data;
+		   next =  null;
+		 }
+		
+	 }
+
+     public void push (int data) {
+ 	     Node newNode=new Node(data);
+ 	     newNode.next=head;
+ 	
+ 	     head=newNode;
+ 	
+     }
+ 
+     public void printList() {
+ 	     System.out.print("LinkedList:");
+ 	     Node temp=head;
+ 	
+ 	     while(temp != null) {
+ 		  System.out.print(temp.data+" -> ");
+ 		  temp = temp.next;
+ 	     }
+     }
 }
