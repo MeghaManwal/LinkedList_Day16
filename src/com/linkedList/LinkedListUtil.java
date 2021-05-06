de2d9f76cce806f2186fbacf607bb53480334654
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-06 07:52:16 +0530

    Added:To delete value in the Linked List using pop method

--- src/com/linkedList/LinkedListUtil.java
+++ src/com/linkedList/LinkedListUtil.java
@@ -11,16 +11,20 @@ public class LinkedListUtil {
 		linkedlist.push(56);
 		
 		linkedlist.printList();
-		System.out.println("\n");
+		System.out.println("\n Head Element before pop: "+ linkedlist.topValue());
+		
+		linkedlist.pop();
+		linkedlist.printList();
+		System.out.println("\n Head Element after pop: "+ linkedlist.topValue());
 		
 		LinkedList linkedlist1 = new LinkedList();
 		linkedlist1.append(70);
 	        linkedlist1.append(30);
 		linkedlist1.append(56);
 		
-		linkedlist1.printList();
-		System.out.println("\n");
 		
+		linkedlist1.printList();
+	
 		LinkedList linkedlist2 = new LinkedList();
 		linkedlist2.append(56);
 		linkedlist2.append(40);
@@ -28,6 +32,8 @@ public class LinkedListUtil {
 		
 		linkedlist2.insertAfter(linkedlist2.head.next, 30);
 		linkedlist2.printList();
+		
+		
 	}
 }
 
@@ -36,31 +42,31 @@ class LinkedList {
 	
      class Node {
 		
-	     int data;
-	     Node next;
+	          int data;
+	          Node next;
 	
-	     Node(int data){
-	       this.data = data;
-	       next =  null;
+	          Node(int data){
+	          this.data = data;
+	          next =  null;
 	     }
      }
 
      public void push (int data) {
- 	     Node newNode=new Node(data);
- 	     newNode.next=head;
+ 	         Node newNode=new Node(data);
+ 	         newNode.next=head;
  	
- 	     head=newNode;
+ 	         head=newNode;
  	
      }
  
      public void printList() {
- 	     System.out.print("LinkedList:");
- 	     Node temp=head;
+ 	         System.out.print("LinkedList:");
+ 	         Node temp=head;
  	
- 	     while(temp != null) {
- 		  System.out.print(temp.data+" -> ");
- 		  temp = temp.next;
- 	     }
+ 	         while(temp != null) {
+ 		       System.out.print(temp.data+" -> ");
+ 		       temp = temp.next;
+ 	         }
      }
      
      public void append(int data) {
@@ -86,5 +92,25 @@ class LinkedList {
     	     newNode.next = prevNode.next;
     	     prevNode.next = newNode;
      }
+     
+     public boolean isEmpty() {
+    	     return head == null;
+     } 
+     
+     public int topValue() {
+    	     if(!isEmpty()) 
+    		   return head.data; 
+    	     else
+    		   return 0;
+     }
+     
+     public void pop() {
+    	     if (head == null) {
+    		  System.out.println("head should not be null");
+    	     }
+             head=(head).next;	 	 
+     }
+     
+     
      
 }
